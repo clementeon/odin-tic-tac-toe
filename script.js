bc5259e5d8b0a7656a50ed5ebffaac4d68be162b
Clement Zhou <clmementzhou2@gmail.com>
2026-07-10 14:08:18 -0400

    ADD functionality to board and scorkeeping ADD formatting to grid and display

--- script.js
+++ script.js
@@ -1,76 +1,184 @@
 const gameboard = (() => {
-	let board = [["", "" ,""], ["", "" ,""], ["", "" ,""]];
+	let board = [
+		["", "", ""],
+		["", "", ""],
+		["", "", ""],
+	];
 
+	let gamesPlayed = 1;
+
+	const addGame = () => {
+		gamesPlayed++;
+	};
+
+	const returnGames = () => {
+		return gamesPlayed;
+	};
 	const addPiece = (pos, piece) => {
-		board[Math.floor(pos / 3)][pos % 2] = piece;
+		board[Math.floor(pos / 3)][pos % 3] = piece;
+	};
+
+	const returnPos = (pos) => {
+		return board[Math.floor(pos / 3)][pos % 3];
 	};
 
 	const returnBoard = () => board;
 
-	return { addPiece, returnBoard };
+	const clearBoard = () => {
+		for (const row of board) {
+			for (let i = 0; i < row.length; i++) {
+				row[i] = "";
+			}
+		}
+	};
+
+	return {
+		addGame,
+		returnGames,
+		addPiece,
+		returnBoard,
+		clearBoard,
+		returnPos,
+	};
 })();
 
 function createPlayer(piece) {
 	let playerPiece = piece;
 	let playerWins = 0;
 
-	return { playerPiece, playerWins };
+	const addWin = () => {
+		playerWins++;
+	};
+
+	const numWins = () => {
+		return playerWins;
+	};
+
+	const playPiece = () => {
+		return playerPiece;
+	};
+	return { numWins, addWin, playPiece };
 }
 
-export const gameController = (() => {
-	const makeMove = (pos, board, player) => {
-		let total = 0;
-		for (const row of board) {
-			total += row.length;
-		}
-		if (total % 2 == 0) {
-			gameboard.addPiece(pos, player.playerPiece);
-		} else {
-			gameController.addPiece(pos, player.playerPiece);
-		}
-	};
-
+const gameController = (() => {
 	function check3(arr) {
-		return arr.every((num) => (num == arr[0]) && (num != ""));
-	};
+		const first = gameboard.returnPos(arr[0]);
+
+		if (first === "") return false;
+
+		return arr.every((index) => gameboard.returnPos(index) === first);
+	}
 
 	const checkWin = (board) => {
-		for (let row of board) {
-			if (check3(row)) {
-				return true;
-			}
-		}
-		for (let i = 0; i < board.length; i++) {
-			let temp = [];
-			for (let row of board) {
+		const winningLines = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
 
-				temp.push(row[i]);
-			}
-			if (check3(temp)) {
-				return true;
-			}
-		}
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
 
-		if (
-			check3(board.map((row, i) => board[i][i])) ||
-			check3(board.map((row, i) => board[i][row.length - 1 - i]))
-		) {
-			return true;
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+
+		for (const line of winningLines) {
+			if (check3(line, board)) {
+				return line;
+			}
 		}
 
 		return false;
 	};
 
-	return { makeMove, checkWin };
+	const checkDraw = (board) => {
+		return board.every((row) => {
+			return row.every((num) => num != "");
+		});
+	};
+	return { check3, checkWin, checkDraw };
 })();
 
 const displayController = (() => {
-	const printBoard = (board) => {
-		for (let row of board) {
-			console.log(row);
-		}
+	const cells = document.querySelectorAll(".cell");
+	const playerX = createPlayer("X");
+	const playerO = createPlayer("O");
+	const gameCount = document.getElementById("total-games");
+	let current = playerX;
+	const next = document.getElementById("next");
+	const winner = document.getElementById("winner");
+
+	const disableCells = () => {
+		cells.forEach((cell) => {
+			cell.disabled = true;
+		});
 	};
-	return { printBoard };
+
+	const enableCells = () => {
+		cells.forEach((cell) => {
+			cell.disabled = false;
+		});
+	};
+
+	cells.forEach((cell, index) => {
+		cell.addEventListener("click", () => {
+			if (gameboard.returnPos(index) == "") {
+				gameboard.addPiece(index, current.playPiece());
+
+				cell.innerHTML = current.playPiece();
+
+				let temp = gameController.checkWin(gameboard.returnBoard());
+				if (temp) {
+					winner.innerText = `${current.playPiece()} Wins`;
+					current.addWin();
+
+					const playerDisplay = document.getElementById(
+						current.playPiece(),
+					);
+					const winsDisplay = playerDisplay.querySelector(".wins");
+					winsDisplay.innerText = `Wins: ${current.numWins()}`;
+					
+					next.style.display = "flex";
+					disableCells();
+				} else if (gameController.checkDraw(gameboard.returnBoard())) {
+					winner.innerText = "Draw";
+					next.style.display = "flex";
+					disableCells();
+				}
+
+				if (current == playerX) {
+					current = playerO;
+				} else {
+					current = playerX;
+				}
+			}
+		});
+	});
+
+	const cleanBoard = () => {
+		gameboard.clearBoard();
+		cells.forEach((cell, index) => {
+			cell.innerHTML = "";
+		});
+		enableCells();
+	};
+
+	document.getElementById("next-button").addEventListener("click", () => {
+		gameboard.addGame();
+		gameCount.innerText = `Game ${gameboard.returnGames()}`;
+		next.style.display = "none";
+		if (gameboard.returnGames() % 2 == 1) {
+			current = playerX;
+		} else {
+			current = playerO;
+		}
+		cleanBoard();
+	});
+
+	document.getElementById("restart").addEventListener("click", () => {
+		cleanBoard();
+	});
+
+	return { cells, current, playerO, playerX };
 })();
-
-
